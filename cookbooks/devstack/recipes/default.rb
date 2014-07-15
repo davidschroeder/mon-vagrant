52e186204e89116cb898eb90692deafaf745b4b1
David Schroeder <david.schroeder@hp.com>
2014-07-15 14:33:30 -0600

    Removed keystone.conf editing from devstack recipe

--- cookbooks/devstack/recipes/default.rb
+++ cookbooks/devstack/recipes/default.rb
@@ -1,21 +1,3 @@
-# Turn a devstack image (https://vagrantcloud.com/cyrusbio/devstack) into
-# something useful to mini-mon
-
-admin_token = 'ADMIN'
-my_ip = '192.168.10.5'
-ks_conf = '/etc/keystone/keystone.conf'
-
-# Modify keystone configuration to...
-## ...bind to all IPs
-execute "sed -i.bak1 's^\\(.*bind_host = \\).*^\\10.0.0.0^' #{ks_conf}"
-# ...set the default admin token
-execute "sed -i.bak2 's^\\(admin_token = \\).*^\\1#{admin_token}^' #{ks_conf}"
-# ...set endpoints that other mini-mon nodes can use
-execute "sed -i.bak3 's^\\(.*endpoint = http://\\).*\\(:.*\\)^\\1#{my_ip}\\2^' #{ks_conf}"
-
-# Restart the keystone process to apply the above changes
-service "keystone-all" do
-    provider Chef::Provider::Service::Upstart
-    action :restart
-end
-
+# As of 2014-07-10, Keystone is no longer run as a daemon, but under Apache.
+# Therefore, this recipe, which prepared keystone.conf for mini-mon use,
+# is no longer necessary, but left in place in case future tweaks are needed.
